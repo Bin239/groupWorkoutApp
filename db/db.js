6047e88ea534226a02478fa81f8da1fedf1d71f0
Binod <binod@Binitas-MacBook-Air.local>
2019-02-01 10:04:28 -0700

    updating code for heroku

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb://localHost/blog'
+const connectionString = process.env.MONGODB_URI;
 
 mongoose.connect(connectionString, {
     useNewUrlParser: true,
